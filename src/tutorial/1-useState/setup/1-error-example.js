14175c72121f8b0c4f8de29c08e82e202c6598d9
João Pedro Bove da Costa <88252025+joao-pbc@users.noreply.github.com>
2024-02-27 15:47:32 -0300

    useState Basics
    Implemented my first use state

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = 'Random Title';
+  const clickHandle = () => {
+    let title = 'change title';
+    console.log({title});
+  };
+  return  (
+    <>
+      <h2>{title}</h2>
+      <button type='button' onClick={clickHandle}>
+        change title
+      </button>
+    </>
+  );
 };
 
 export default ErrorExample;
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  
+  const [text, setText] = useState('random title');
+  const clickHandler = () => {
+    setText('new Title')
+  }
+  return (
+    <>
+      <h2>{text}</h2>
+      <button type='button' onClick = {clickHandler}>
+        change title
+      </button>
+    </>
+  
+  );
 };
 
 export default UseStateBasics;
