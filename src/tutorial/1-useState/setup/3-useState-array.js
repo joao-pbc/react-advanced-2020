6689d28d0c1bbf135cdf39b3032706b6298efe62
João Pedro Bove da Costa <88252025+joao-pbc@users.noreply.github.com>
2024-02-28 18:59:05 -0300

    useState with Arrays
    First part o implementing useState with arrays

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { data } from '../../../data';
 
+
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <>
+      {people.map((people) => {
+        const {id, name} = people;
+          return <h2 key = {id} >{ name }</h2>
+        }
+      )
+      }
+    </>
+  )
 };
 
 export default UseStateArray;
